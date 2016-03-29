b56b73f4178c8846e7ea5495ef608f81ad70e0b2
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-03-29 12:41:36 -0400

    Add in Boot App to run server.

--- server/src/main/scala/demo/server/Boot.scala
+++ server/src/main/scala/demo/server/Boot.scala
@@ -1,8 +1,29 @@
 package demo.server
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import demo.server.artist.{ArtistDatabase, ArtistResource, ArtistService}
+import demo.server.song.{SongDatabase, SongResource, SongService}
+import akka.http.scaladsl.server.Directives._
+
 /**
   * Created by hollinwilkins on 3/29/16.
   */
-class Boot {
+object Boot extends App {
+  implicit val system = ActorSystem("akka-http-demo")
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
 
+  val artistDb = new ArtistDatabase()
+  val artistService = ArtistService(artistDb)
+  val artistResource = ArtistResource(artistService)
+
+  val songDb = new SongDatabase()
+  val songService = SongService(songDb)
+  val songResource = SongResource(songService)
+
+  val routes = artistResource.routes ~ songResource.routes
+
+  Http().bindAndHandle(routes, "localhost", 8080)
 }
